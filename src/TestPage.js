80c526add49fea6850b6b84d5abc145b2c6fdfb3
logeshhy <logeshhy@gmail.com>
2020-01-04 21:59:23 -0800

    Initial commit

---
+++ src/TestPage.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Content() {
+  return <h1>I am Test Page</h1>;
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ResponsiveDrawer from "./MiniDrawer";
+
+ReactDOM.render(<ResponsiveDrawer />, document.querySelector("#root"));
